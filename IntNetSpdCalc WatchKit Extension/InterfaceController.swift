3362eef789aa6a6d939b2726ff76649a4df3b50a
Nathaniel Konkel <f00314960@s114b-04.cs.cuw.edu>
2015-10-28 14:17:02 -0500

    Initial Commit

---
+++ IntNetSpdCalc WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  IntNetSpdCalc WatchKit Extension
+//
+//  Created by Nathaniel Konkel on 10/28/15.
+//  Copyright © 2015 Nathaniel Konkel. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awakeWithContext(context: AnyObject?) {
+        super.awakeWithContext(context)
+        
+        // Configure interface objects here.
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
